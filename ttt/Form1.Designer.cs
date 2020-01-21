6131d446df4dff5d5b63df304af66981b3b56aad
Makula <hubert.makula@delphi.com>
2020-01-21 12:45:24 +0100

    ttt

---
+++ ttt/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ttt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.b5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.resetButton, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.CloseButton, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(333, 351);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(86, 90);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(75, 78);
+            this.b5.TabIndex = 18;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.B5_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(169, 3);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 78);
+            this.b3.TabIndex = 17;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(86, 3);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 78);
+            this.b2.TabIndex = 16;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(3, 3);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 78);
+            this.b1.TabIndex = 15;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(169, 90);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(75, 78);
+            this.b6.TabIndex = 14;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.B6_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(3, 90);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(75, 78);
+            this.b4.TabIndex = 13;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.B4_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(3, 177);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(75, 78);
+            this.b7.TabIndex = 12;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.B7_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(252, 3);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(169, 177);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(75, 78);
+            this.b9.TabIndex = 9;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.B9_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(86, 177);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(75, 78);
+            this.b8.TabIndex = 10;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.B8_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(3, 264);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 19;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 375);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ ttt/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ttt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //zmienna publiczna
+        public int ruch = 3;
+
+        //Tablica przechowuje informacje , czy pole jest zajete
+        Boolean[] occupited = new Boolean[9];
+
+        //tablica przechowuje informacje, ktory gracz zaja dane pole
+        //1-krzyzyk, 2- kolo
+        int[] ttt = new int[9];
+
+        //Blokowanie przyciskow
+        public void blockClick()
+        {
+            for(int i = 0; i<9; i++)
+            {
+                occupited[i] = true;
+            }
+        }
+
+        public void spr()
+        {
+            if (ttt[0] == 1 && ttt[1] == 1 && ttt[2] == 1)
+            {
+                b1.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                b3.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if(ttt[3] == 1 && ttt[4] == 1 && ttt[5] == 1)
+            {
+                b4.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b6.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[6] == 1 && ttt[7] == 1 && ttt[8] == 1)
+            {
+                b7.BackColor = Color.Red;
+                b8.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[0] == 1 && ttt[3] == 1 && ttt[6] == 1)
+            {
+                b1.BackColor = Color.Red;
+                b4.BackColor = Color.Red;
+                b7.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[1] == 1 && ttt[4] == 1 && ttt[7] == 1)
+            {
+                b2.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b8.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[2] == 1 && ttt[5] == 1 && ttt[8] == 1)
+            {
+                b3.BackColor = Color.Red;
+                b6.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[0] == 1 && ttt[4] == 1 && ttt[8] == 1)
+            {
+                b1.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[2] == 1 && ttt[4] == 1 && ttt[6] == 1)
+            {
+                b3.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b7.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[0] == 2 && ttt[1] == 2 && ttt[2] == 2)
+            {
+                b1.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                b3.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[3] == 2 && ttt[4] == 2 && ttt[5] == 2)
+            {
+                b4.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b6.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[6] == 2 && ttt[7] == 2 && ttt[8] == 2)
+            {
+                b7.BackColor = Color.Red;
+                b8.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[0] == 2 && ttt[3] == 2 && ttt[6] == 2)
+            {
+                b1.BackColor = Color.Red;
+                b4.BackColor = Color.Red;
+                b7.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[1] == 2 && ttt[4] == 2 && ttt[7] == 2)
+            {
+                b2.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b8.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[2] == 2 && ttt[5] == 2 && ttt[8] == 2)
+            {
+                b3.BackColor = Color.Red;
+                b6.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[0] == 2 && ttt[4] == 2 && ttt[8] == 2)
+            {
+                b1.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b9.BackColor = Color.Red;
+
+                blockClick();
+            }
+            else if (ttt[2] == 2 && ttt[4] == 2 && ttt[6] == 2)
+            {
+                b3.BackColor = Color.Red;
+                b5.BackColor = Color.Red;
+                b7.BackColor = Color.Red;
+
+                blockClick();
+            }
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            if(occupited[0] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b1.Text = "X";
+                    ttt[0] = 1;
+                }
+                else
+                {
+                    b1.Text = "O";
+                    ttt[0] = 2;
+                }
+                occupited[0] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            if (occupited[1] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b2.Text = "X";
+                    ttt[1] = 1;
+                }
+                else
+                {
+                    b2.Text = "O";
+                    ttt[1] = 2;
+                }
+                occupited[1] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            if (occupited[2] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b3.Text = "X";
+                    ttt[2] = 1;
+                }
+                else
+                {
+                    b3.Text = "O";
+                    ttt[2] = 2;
+                }
+                occupited[2] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B4_Click(object sender, EventArgs e)
+        {
+            if (occupited[3] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b4.Text = "X";
+                    ttt[3] = 1;
+                }
+                else
+                {
+                    b4.Text = "O";
+                    ttt[3] = 2;
+                }
+                occupited[3] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B5_Click(object sender, EventArgs e)
+        {
+            if (occupited[4] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b5.Text = "X";
+                    ttt[4] = 1;
+                }
+                else
+                {
+                    b5.Text = "O";
+                    ttt[4] = 2;
+                }
+                occupited[4] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B6_Click(object sender, EventArgs e)
+        {
+            if (occupited[5] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b6.Text = "X";
+                    ttt[5] = 1;
+                }
+                else
+                {
+                    b6.Text = "O";
+                    ttt[5] = 2;
+                }
+                occupited[5] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B7_Click(object sender, EventArgs e)
+        {
+            if (occupited[6] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b7.Text = "X";
+                    ttt[6] = 1;
+                }
+                else
+                {
+                    b7.Text = "O";
+                    ttt[6] = 2;
+                }
+                occupited[6] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B8_Click(object sender, EventArgs e)
+        {
+            if (occupited[7] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b8.Text = "X";
+                    ttt[7] = 1;
+                }
+                else
+                {
+                    b8.Text = "O";
+                    ttt[7] = 2;
+                }
+                occupited[7] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void B9_Click(object sender, EventArgs e)
+        {
+            if (occupited[8] == false)
+            {
+                if (ruch % 2 == 1)
+                {
+                    b9.Text = "X";
+                    ttt[8] = 1;
+                }
+                else
+                {
+                    b9.Text = "O";
+                    ttt[8] = 2;
+                }
+                occupited[8] = true;
+            }
+            spr();
+            ruch++;
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ruch = 3;
+            for(int i=0; i<9; i++)
+            {
+                occupited[i] = false;
+                ttt[i] = 0;
+                b1.Text = ""; b1.BackColor = Color.White;
+                b2.Text = ""; b2.BackColor = Color.White;
+                b3.Text = ""; b3.BackColor = Color.White;
+                b4.Text = ""; b4.BackColor = Color.White;
+                b5.Text = ""; b5.BackColor = Color.White;
+                b6.Text = ""; b6.BackColor = Color.White;
+                b7.Text = ""; b7.BackColor = Color.White;
+                b8.Text = ""; b8.BackColor = Color.White;
+                b9.Text = ""; b9.BackColor = Color.White;
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        
+    }
+}
